d8393fb1c87f5b705370c720ee1a415c15a9fc9e
tuongmengleang <tuong.mengleang@onesala.com>
2021-12-17 10:21:26 +0700

    remove conversation cache

--- src/services/conversation.service.js
+++ src/services/conversation.service.js
@@ -7,9 +7,19 @@ const Conversation = require('../models/Conversation');
  */
 const updateConversation = async (conversation_id) => {
     await Conversation.findOneAndUpdate({ _id: conversation_id }, { updatedAt: Date.now() })
-    // console.log("conversation :", conversation)
 };
+
+/**
+ * Find conversation by userId
+ * @param {ObjectId} userId
+ * @returns {Promise<Conversation>}
+ */
+const findConversation = async (creator, userId) => {
+    const result = await Conversation.findOne({ participants : { '$all': [userId, creator.toString()] } });
+    return result
+}
 
 module.exports = {
     updateConversation,
+    findConversation
 };
